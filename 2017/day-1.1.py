e0f39179929a382986f49d021b8f7097cbf56e35
Jitesh Pubreja <jitesh.pubreja@yahoo.com>
2021-09-20 05:15:41 +0530

    Updated Day 1 Part 1 2017 Code

--- 2017/day-1.1.py
+++ 2017/day-1.1.py
@@ -10,12 +10,7 @@ data = [x.strip() for x in list(data) if x.strip()]
 total = 0
 
 for i,x in enumerate(data):
-  if i == len(data)-1:
-    if data[i] == data[0]:
-      total += int(data[i])
-  else:
-    if data[i] == data[i+1]:
-      total += int(data[i])
-  
+  if data[i-1] == data[i]:
+    total += int(data[i])
 
 print(total)
